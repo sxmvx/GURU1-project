4dde4495b3f963f9e5a16b41bc8143ff9445fab9
LeeYeso <leeyeso0315@gmail.com>
2022-02-05 05:33:20 +0900

    미니게임 수정
    미니게임들 세부적인 부분은 더 수정해야하지만 전체적인 부분은 완성했습니다.

--- Assets/Maze/Scripts/Button.cs
+++ Assets/Maze/Scripts/Button.cs
@@ -4,48 +4,51 @@ using UnityEngine;
 
 public class Button : MonoBehaviour //오브젝트 변수명 정리하기
 {
-    [SerializeField] GameObject obj;
-    [SerializeField] GameObject obj2;
-    [SerializeField] GameObject obj3;
-    [SerializeField] GameObject obj4;
-    [SerializeField] GameObject obj5;
-    [SerializeField] GameObject obj6;
-    public UnityEngine.UI.Text MainText;
+    [SerializeField] GameObject GameTitle;
+    [SerializeField] GameObject Start1;
+    [SerializeField] GameObject BlackBackground;
+    [SerializeField] GameObject GameRule;
+    [SerializeField] GameObject GameRuleScreen;
+    [SerializeField] GameObject start2;
+    [SerializeField] GameObject GameText;
+
 
 
     void Start()
     {
-        obj6.gameObject.SetActive(false);
-        obj5.gameObject.SetActive(false);
-        MainText.gameObject.SetActive(false);
+        start2.gameObject.SetActive(false);
+        GameRuleScreen.gameObject.SetActive(false);
+        GameText.gameObject.SetActive(false);
     }
    
     public void OnclickStart()
     {
-        Destroy(obj);
-        Destroy(obj2);
-        Destroy(obj3);
-        Destroy(obj4);
-        obj6.gameObject.SetActive(false);
-        obj5.gameObject.SetActive(false);
-        MainText.gameObject.SetActive(true);
+        Destroy(GameTitle);
+        Destroy(Start1);
+        Destroy(BlackBackground);
+        Destroy(GameRule);
+        start2.gameObject.SetActive(false);
+        GameRuleScreen.gameObject.SetActive(false);
+        GameText.gameObject.SetActive(true);
 
     }
     public void OnclickRule()
     {
-        Destroy(obj);
-        Destroy(obj2);
-        Destroy(obj4);
-        obj6.gameObject.SetActive(true);
-        obj5.gameObject.SetActive(true);
+        Destroy(GameTitle);
+        Destroy(Start1);
+        Destroy(GameRule);
+        start2.gameObject.SetActive(true);
+        GameRuleScreen.gameObject.SetActive(true);
 
     }
     public void OnclickStart2()
     {
-        Destroy(obj3);
-        Destroy(obj5);
-        Destroy(obj6);
-        MainText.gameObject.SetActive(true);
+        Destroy(BlackBackground);
+        Destroy(GameRuleScreen);
+        Destroy(start2);
+        GameText.gameObject.SetActive(true);
+
+
     }
    
 }
